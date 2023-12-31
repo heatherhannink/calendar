2dbbd4b85c07ad57e838bbcda9cb789824657ed8
Heather <144755242+heatherhannink@users.noreply.github.com>
2023-12-30 22:39:59 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//create schedule//
+let date = dayjs() ("DD, MMMM, YYYY");
+console.log (date);
+//current day is displayed at top when you open//
+
+//present time blocks for 9-5//
+
+//color code time blocks for past, present and future//
+
+//when you click timeblock you can enter event//
+
+//when i click save text is saved into local storage//
+
+//when i hit refresh saved event persists//
+
+//npm install dayjs//
+//# or//
+//yarn add dayjs//
+//# or//
+//pnpm add dayjs//
+
