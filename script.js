f5ceae890a722cd51750ba988245e16bdb925909
Heather <144755242+heatherhannink@users.noreply.github.com>
2023-12-31 23:31:40 -0500

    add js code, color block, time blocks

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
 //create schedule//
-let date = dayjs() ("DD, MMMM, YYYY");
+const hoursArr = document.querySelectorAll(".time-block");
+
+const now =dayjs().format ("H")
+
+//get the squares color coded//
+for (let i=0; i<hoursArr.length; i++){
+    const hour = hoursArr[i].id.substring(5)
+
+    if(now === hour){
+        hoursArr[i].setAttribute('class, row time-block present')
+    }
+}    
+
+
+
+
+
+// save data to local storage - done
+
+const buttonArr = document.querySelectorAll('button')
+const textAreaArr = document.querySelectorAll('.description')
+
+for (let i = 0; i < buttonArr.length; i++){
+    const clickHandler = () => {
+        const text = textAreaArr[i].value;
+        localStorage.setItem(i, text)
+
+    }
+
+    buttonArr[i].addEventListener('click', clickHandler)
+}
+
+
+// get data from local storage
+
+// loop
+
+// localStorage.getItem()
+
+//textAreaArr[i].value = whatever came back from local
+
+
+
+var currentHour = dayjs().hour();
+let date = dayjs().format ("DD, MMMM, YYYY");
 console.log (date);
 //current day is displayed at top when you open//
+let currentDay = document.querySelector ('#currentDay');
+
+let paragraph = document.createElement("p");
+paragraph.innerText = date;
+currentDay.appendChild(paragraph);
 
 //present time blocks for 9-5//
 
@@ -10,12 +59,8 @@ console.log (date);
 //when you click timeblock you can enter event//
 
 //when i click save text is saved into local storage//
-
+//localStorage.setItem()//
 //when i hit refresh saved event persists//
+//localStorage.getItem()//
 
-//npm install dayjs//
-//# or//
-//yarn add dayjs//
-//# or//
-//pnpm add dayjs//
 
